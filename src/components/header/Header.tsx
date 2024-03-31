1aaa6ca80042f97bb98ebc392d14934143eca0ff
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2024-03-31 20:34:17 -0300

    Implementing item quantity on header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,31 +2,40 @@ import Image from "next/image"
 import { TextMd, TextSm, Headline } from "../typography/TypographyElements"
 import { CartContainer, HeaderWrapper, MyCartContainer } from "./HeaderElements"
 import Link from "next/link"
+import { useGlobal } from "@/hooks/global-hook"
 
 const Header = (): JSX.Element => {
-  const itemsQty = 1 // for now
+  const { cartItems } = useGlobal()
+
+  const itemsQty = Object.values(cartItems).reduce(
+    (total, item) => total + item.qty,
+    0
+  )
+
   return (
     <>
       <HeaderWrapper>
         <Link href="/">
           <Headline>WeMovies</Headline>
         </Link>
-        <CartContainer>
-          <MyCartContainer>
-            <TextMd className="header__my-cart-text" variation="primary">
-              Meu carrinho
-            </TextMd>
-            <TextSm variation="tertiary" fontWeight={600}>
-              {itemsQty} itens
-            </TextSm>
-          </MyCartContainer>
-          <Image
-            alt="Shopping bag"
-            src="/shopping-bag.svg"
-            width={24}
-            height={20.5}
-          />
-        </CartContainer>
+        <Link href="/cart">
+          <CartContainer>
+            <MyCartContainer>
+              <TextMd className="header__my-cart-text" variation="primary">
+                Meu carrinho
+              </TextMd>
+              <TextSm variation="tertiary" fontWeight={600}>
+                {itemsQty} {itemsQty === 1 ? "item" : "itens"}
+              </TextSm>
+            </MyCartContainer>
+            <Image
+              alt="Shopping bag"
+              src="/shopping-bag.svg"
+              width={24}
+              height={20.5}
+            />
+          </CartContainer>
+        </Link>
       </HeaderWrapper>
     </>
   )
