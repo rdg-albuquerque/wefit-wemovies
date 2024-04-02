92ce29c5f78fbc246dc65ba4f91833107ae345f4
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2024-04-01 22:17:50 -0300

    Improvement on formatPrice function

--- src/utils/format-price.ts
+++ src/utils/format-price.ts
@@ -1,5 +1,8 @@
-export const formatPrice = (rawPrice: string) => {
-  return Number(rawPrice).toLocaleString("pt-BR", {
+export const formatPrice = (rawPrice: string | number, qty: number = 1) => {
+  const rawTotal =
+    typeof rawPrice === "string" ? Number(rawPrice) * qty : rawPrice * qty
+
+  return rawTotal.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
   })
